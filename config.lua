ef869909bdb51c47a71ba081940aa98476709f56
Breno Peres <brenoperes@centralx.com>
2022-07-06 17:16:34 -0300

    Desliga as opções de itálico para o tema

--- config.lua
+++ config.lua
@@ -209,7 +209,16 @@ end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- Additional Plugins
 lvim.plugins = {
-  { "rebelot/kanagawa.nvim" },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function ()
+      require("kanagawa").setup({
+        commentStyle = { italic = false },
+        keywordStyle = { italic = false },
+        variablebuiltinStyle = { italic = false },
+      })
+    end
+  },
   -- Neovim motions on speed!
   {
     "phaazon/hop.nvim",
